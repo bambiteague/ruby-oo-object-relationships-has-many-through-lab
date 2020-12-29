aeb7c186708302a12c877043c20fdc970c215486
bambi teague <teeegsa@gmail.com>
2020-12-29 12:31:38 -0600

    commit tree 06e52b24cb18ffa01ff711c3c0641bb0ce280329

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,29 @@
 
+class Doctor
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select do |appointments|
+      appointments.doctor == self
+    end
+  end
+
+  def patients
+    self.appointments.collect {|appointment| appointment.patient }
+  end
+
+end
